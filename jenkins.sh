9eba8398a0fdfb59068c5fa5ff7d38937bc641c5
EC2 Default User <halilyvz2020@gmail.com>
2020-08-26 03:12:55 +0000

    new files copied

--- jenkins.sh
+++ jenkins.sh
@@ -4,4 +4,4 @@
             "sudo yum install   jenkins-{{user `jenkins_version`}} -y",
             "sudo yum install   java-{{user `java_version`}}-openjdk-devel -y",
             "sudo systemctl start jenkins",
-            "sudo systemctl enable jenkins"
+            "sudo systemctl status jenkins"
